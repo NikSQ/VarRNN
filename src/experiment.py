added tensorboard bug fix: variance is now constrained to be positive

src/experiment.py
@@ -79,10 +79,6 @@ class Experiment:
                          feed_dict={self.labelled_data.x_va_placeholder: self.data_dict['x_va'],
                                     self.labelled_data.y_va_placeholder: self.data_dict['y_va']})
 
-                grads = sess.run(self.rnn.gradients, feed_dict={self.labelled_data.batch_counter: 0})
-                for idx, grad in enumerate(grads):
-                    print('IDX: {}, Shape: {}, Shape2: {}, Compare: {} - {}'.format(idx, grad[0].shape, grad[1].shape, grad[0][0], grad[1][0]))
-
                 for epoch in range(max_epochs):
                     if epoch % info_config['calc_performance_every'] == 0:
                         tr_acc, tr_loss, va_acc, va_loss = self.store_performance(sess, info_config, result_dict, epoch)
@@ -98,6 +94,15 @@ class Experiment:
                                                                    self.labelled_data.batch_counter: minibatch_idx})
                     else:
                         sess.run(self.rnn.train_op, feed_dict={self.rnn.learning_rate: training_config['learning_rate']})
+                        #grads = sess.run(self.rnn.gradients, feed_dict={self.labelled_data.batch_counter: 0})
+                        #for idx, g in enumerate(grads):
+                            #print("IDX: {}, NAN: {}, SHAPE: {}".format(idx, sum(np.isnan(g[0])), g[0].shape))
+                            #print("IDX: {}, NAN: {}, SHAPE: {}".format(idx, sum(np.isnan(g[1])), g[1].shape))
+
+
+                        print(sess.run(self.rnn.a, feed_dict={self.labelled_data.batch_counter: 0}))
+                        print(sess.run(self.rnn.b, feed_dict={self.labelled_data.batch_counter: 0}))
+                        print(sess.run(self.rnn.c, feed_dict={self.labelled_data.batch_counter: 0}))
 
                     if info_config['tensorboard']['is_enabled'] \
                             and current_epoch % info_config['tensorboard']['period'] == 0:

src/fc_layer.py
@@ -16,12 +16,12 @@ class FCLayer:
         with tf.variable_scope(self.layer_config['var_scope']):
             self.w_m = tf.get_variable(name='w_m', shape=self.w_shape,
                                        initializer=get_mean_initializer(self.layer_config['w'], self.w_shape))
-            self.w_v = tf.get_variable(name='w_v', shape=self.w_shape,
-                                       initializer=get_var_initializer(self.layer_config['w'], self.w_shape))
+            self.w_v = tf.exp(tf.get_variable(name='w_v', shape=self.w_shape,
+                                       initializer=get_var_initializer(self.layer_config['w'], self.w_shape)))
             self.b_m = tf.get_variable(name='b_m', shape=self.b_shape,
                                        initializer=get_mean_initializer(self.layer_config['b'], self.b_shape))
-            self.b_v = tf.get_variable(name='b_v', shape=self.b_shape,
-                                       initializer=get_var_initializer(self.layer_config['b'], self.b_shape))
+            self.b_v = tf.exp(tf.get_variable(name='b_v', shape=self.b_shape,
+                                       initializer=get_var_initializer(self.layer_config['b'], self.b_shape)))
 
             self.kl_loss = get_kl_loss(self.layer_config['w'], self.w_m, self.w_v) + \
                 get_kl_loss(self.layer_config['b'], self.b_m, self.b_v)

src/lstm_layer.py
@@ -16,39 +16,39 @@ class LSTMLayer:
         with tf.variable_scope(self.layer_config['var_scope']):
             self.wf_m = tf.get_variable(name='wf_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wf'], self.w_shape))
-            self.wf_v = tf.get_variable(name='wf_v', shape=self.w_shape,
-                                        initializer=get_var_initializer(self.layer_config['wf'], self.w_shape))
+            self.wf_v = tf.exp(tf.get_variable(name='wf_v', shape=self.w_shape,
+                                        initializer=get_var_initializer(self.layer_config['wf'], self.w_shape)))
             self.bf_m = tf.get_variable(name='bf_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bf'], self.b_shape))
-            self.bf_v = tf.get_variable(name='bf_v', shape=self.b_shape,
-                                        initializer=get_var_initializer(self.layer_config['bf'], self.b_shape))
+            self.bf_v = tf.exp(tf.get_variable(name='bf_v', shape=self.b_shape,
+                                        initializer=get_var_initializer(self.layer_config['bf'], self.b_shape)))
 
             self.wi_m = tf.get_variable(name='wi_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wi'], self.w_shape))
-            self.wi_v = tf.get_variable(name='wi_v', shape=self.w_shape,
-                                        initializer=get_var_initializer(self.layer_config['wi'], self.w_shape))
+            self.wi_v = tf.exp(tf.get_variable(name='wi_v', shape=self.w_shape,
+                                        initializer=get_var_initializer(self.layer_config['wi'], self.w_shape)))
             self.bi_m = tf.get_variable(name='bi_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bi'], self.b_shape))
-            self.bi_v = tf.get_variable(name='bi_v', shape=self.b_shape,
-                                        initializer=get_var_initializer(self.layer_config['bi'], self.b_shape))
+            self.bi_v = tf.exp(tf.get_variable(name='bi_v', shape=self.b_shape,
+                                        initializer=get_var_initializer(self.layer_config['bi'], self.b_shape)))
 
             self.wc_m = tf.get_variable(name='wc_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wc'], self.w_shape))
-            self.wc_v = tf.get_variable(name='wc_v', shape=self.w_shape,
-                                        initializer=get_var_initializer(self.layer_config['wc'], self.w_shape))
+            self.wc_v = tf.exp(tf.get_variable(name='wc_v', shape=self.w_shape,
+                                        initializer=get_var_initializer(self.layer_config['wc'], self.w_shape)))
             self.bc_m = tf.get_variable(name='bc_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bc'], self.b_shape))
-            self.bc_v = tf.get_variable(name='bc_v', shape=self.b_shape,
-                                        initializer=get_var_initializer(self.layer_config['bc'], self.b_shape))
+            self.bc_v = tf.exp(tf.get_variable(name='bc_v', shape=self.b_shape,
+                                        initializer=get_var_initializer(self.layer_config['bc'], self.b_shape)))
 
             self.wo_m = tf.get_variable(name='wo_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wo'], self.w_shape))
-            self.wo_v = tf.get_variable(name='wo_v', shape=self.w_shape,
-                                        initializer=get_var_initializer(self.layer_config['wo'], self.w_shape))
+            self.wo_v = tf.exp(tf.get_variable(name='wo_v', shape=self.w_shape,
+                                        initializer=get_var_initializer(self.layer_config['wo'], self.w_shape)))
             self.bo_m = tf.get_variable(name='bo_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bo'], self.b_shape))
-            self.bo_v = tf.get_variable(name='bo_v', shape=self.b_shape,
-                                        initializer=get_var_initializer(self.layer_config['bo'], self.b_shape))
+            self.bo_v = tf.exp(tf.get_variable(name='bo_v', shape=self.b_shape,
+                                        initializer=get_var_initializer(self.layer_config['bo'], self.b_shape)))
 
             summary_ops = []
             summary_ops.append(tf.summary.histogram('wf_m', self.wf_m))

src/rnn.py
@@ -9,6 +9,7 @@ import copy
 
 class RNN:
     def __init__(self, rnn_config, labelled_data):
+        self.c = None
         self.rnn_config = rnn_config
         self.labelled_data = labelled_data
         self.layers = []
@@ -75,14 +76,14 @@ class RNN:
         v_output = tf.cast(tf.concat(v_outputs, axis=2), dtype=tf.float64)
 
         if self.rnn_config['output_type'] == 'classification':
-            smax = tf.nn.softmax(logits=m_output, axis=1) + np.finfo(np.float64).eps
+            smax = tf.nn.softmax(logits=m_output, axis=1)
             t = tf.argmax(y, axis=1)
             batch_range = np.arange(y_shape[0])
             seq_range = np.arange(y_shape[2])
             seqq, batchh = np.meshgrid(seq_range, batch_range)
             g_indices = tf.concat([np.expand_dims(batchh, axis=2), tf.expand_dims(t, axis=2),
                                   np.expand_dims(seqq, axis=2)], axis=2)
-            e_log_likelihood = tf.log(tf.reduce_mean(tf.gather_nd(smax, g_indices))) - \
+            e_log_likelihood = tf.log(np.finfo(np.float64).eps + tf.reduce_mean(tf.gather_nd(smax, g_indices))) - \
                 0.5 * tf.reduce_mean(tf.reduce_sum(tf.multiply(v_output, tf.multiply(smax, 1 - smax)), axis=1))
             kl_loss = 0
             for layer in self.layers:
@@ -108,7 +109,7 @@ class RNN:
 
             gradient_summaries = []
             for layer_idx in range(len(self.gradients)):
-                gradient_summaries.append(tf.summary.histogram('gradients' + str(layer_idx),
+                gradient_summaries.append(tf.summary.histogram('gradients',
                                                                self.gradients[layer_idx][0]))
             self.gradient_summaries = tf.summary.merge(gradient_summaries)
 