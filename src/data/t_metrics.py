temporarily disabled variance (set to zero); fixed wrong implementation of lstm cell in local reparametrization

src/data/t_metrics.py
@@ -51,9 +51,9 @@ class TMetrics:
         self.result_dict.update({process_key: {'nelbo': [], 'kl': [], 'elogl': [], 'acc': []}})
         self.op_dict.update({process_key: [nelbo_op, kl_op, elogl_op, accs_op]})
 
-    def add_s_vars(self, dict_key, sample_op, loss_op, accs_op):
-        self.result_dict.update({dict_key: {'m_loss': [], 's_loss': [], 'm_acc': [], 's_acc': []}})
-        self.op_dict.update({dict_key: {'metrics': [loss_op, accs_op], 'sample': sample_op}})
+    def add_s_vars(self, process_key, sample_op, loss_op, accs_op):
+        self.result_dict.update({process_key: {'m_loss': [], 's_loss': [], 'm_acc': [], 's_acc': []}})
+        self.op_dict.update({process_key: {'metrics': [loss_op, accs_op], 'sample': sample_op}})
 
     # Retrieves metrics of the performance of the processes which were added using add_vars()
     # A process is a method of operating a RNN (bayesian or sampled weights) combined with a dataset
@@ -95,6 +95,7 @@ class TMetrics:
             cum_acc = 0
             sess.run(self.op_dict[process_key]['sample'])
             for minibatch_idx in range(self.l_data.data[data_key]['n_minibatches']):
+                minibatch_idx = 0
                 loss, acc = sess.run(self.op_dict[process_key]['metrics'],
                                      feed_dict={self.l_data.batch_idx: minibatch_idx})
                 cum_loss += loss
@@ -103,6 +104,7 @@ class TMetrics:
             losses.append(cum_loss / self.l_data.data[data_key]['n_minibatches'])
         losses = np.asarray(losses)
         accs = np.asarray(accs)
+        print(accs)
         self.result_dict[process_key]['m_loss'].append(np.mean(losses))
         self.result_dict[process_key]['s_loss'].append(np.std(losses, ddof=1))
         self.result_dict[process_key]['m_acc'].append(np.mean(accs))

src/lstm_layer.py
@@ -15,39 +15,39 @@ class LSTMLayer:
         with tf.variable_scope(self.layer_config['var_scope']):
             self.wf_m = tf.get_variable(name='wf_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wf'], self.w_shape))
-            self.wf_v = tf.exp(tf.get_variable(name='wf_v', shape=self.w_shape,
-                                               initializer=get_var_initializer(self.layer_config['wf'], self.w_shape)))
+            self.wf_v = tf.get_variable(name='wf_v', shape=self.w_shape,
+                                               initializer=get_var_initializer(self.layer_config['wf'], self.w_shape), trainable=False)
             self.bf_m = tf.get_variable(name='bf_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bf'], self.b_shape))
-            self.bf_v = tf.exp(tf.get_variable(name='bf_v', shape=self.b_shape,
-                                               initializer=get_var_initializer(self.layer_config['bf'], self.b_shape)))
+            self.bf_v = tf.get_variable(name='bf_v', shape=self.b_shape,
+                                               initializer=get_var_initializer(self.layer_config['bf'], self.b_shape), trainable=False)
 
             self.wi_m = tf.get_variable(name='wi_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wi'], self.w_shape))
-            self.wi_v = tf.exp(tf.get_variable(name='wi_v', shape=self.w_shape,
-                                               initializer=get_var_initializer(self.layer_config['wi'], self.w_shape)))
+            self.wi_v = tf.get_variable(name='wi_v', shape=self.w_shape,
+                                               initializer=get_var_initializer(self.layer_config['wi'], self.w_shape), trainable=False)
             self.bi_m = tf.get_variable(name='bi_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bi'], self.b_shape))
-            self.bi_v = tf.exp(tf.get_variable(name='bi_v', shape=self.b_shape,
-                                               initializer=get_var_initializer(self.layer_config['bi'], self.b_shape)))
+            self.bi_v = tf.get_variable(name='bi_v', shape=self.b_shape,
+                                               initializer=get_var_initializer(self.layer_config['bi'], self.b_shape), trainable=False)
 
             self.wc_m = tf.get_variable(name='wc_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wc'], self.w_shape))
-            self.wc_v = tf.exp(tf.get_variable(name='wc_v', shape=self.w_shape,
-                                               initializer=get_var_initializer(self.layer_config['wc'], self.w_shape)))
+            self.wc_v = tf.get_variable(name='wc_v', shape=self.w_shape,
+                                               initializer=get_var_initializer(self.layer_config['wc'], self.w_shape), trainable=False)
             self.bc_m = tf.get_variable(name='bc_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bc'], self.b_shape))
-            self.bc_v = tf.exp(tf.get_variable(name='bc_v', shape=self.b_shape,
-                                               initializer=get_var_initializer(self.layer_config['bc'], self.b_shape)))
+            self.bc_v = tf.get_variable(name='bc_v', shape=self.b_shape,
+                                               initializer=get_var_initializer(self.layer_config['bc'], self.b_shape), trainable=False)
 
             self.wo_m = tf.get_variable(name='wo_m', shape=self.w_shape,
                                         initializer=get_mean_initializer(self.layer_config['wo'], self.w_shape))
-            self.wo_v = tf.exp(tf.get_variable(name='wo_v', shape=self.w_shape,
-                                               initializer=get_var_initializer(self.layer_config['wo'], self.w_shape)))
+            self.wo_v = tf.get_variable(name='wo_v', shape=self.w_shape,
+                                               initializer=get_var_initializer(self.layer_config['wo'], self.w_shape), trainable=False)
             self.bo_m = tf.get_variable(name='bo_m', shape=self.b_shape,
                                         initializer=get_mean_initializer(self.layer_config['bo'], self.b_shape))
-            self.bo_v = tf.exp(tf.get_variable(name='bo_v', shape=self.b_shape,
-                                               initializer=get_var_initializer(self.layer_config['bo'], self.b_shape)))
+            self.bo_v = tf.get_variable(name='bo_v', shape=self.b_shape,
+                                               initializer=get_var_initializer(self.layer_config['bo'], self.b_shape), trainable=False)
 
             self.wf = tf.get_variable(name='wf', shape=self.w_shape, initializer=get_xavier_initializer(self.w_shape))
             self.bf = tf.get_variable(name='bf', shape=self.b_shape,
@@ -166,7 +166,7 @@ class LSTMLayer:
         o = tf.nn.sigmoid(a_o)
 
         self.cell_state_m = tf.multiply(f, self.cell_state_m) + tf.multiply(i, c)
-        self.cell_output_m = tf.multiply(tf.tanh(c), o)
+        self.cell_output_m = tf.multiply(tf.tanh(self.cell_state_m), o)
         return self.cell_output_m
 
     def create_fp(self, x, init_cell_state):

src/rnn.py
@@ -145,7 +145,7 @@ class RNN:
                 scaled_kl = tf.cast(kl, tf.float64) / (self.rnn_config['data_multiplier'] *
                                                        self.l_data.l_data_config[data_key]['minibatch_size'] *
                                                        self.l_data.data[data_key]['n_minibatches'])
-                nelbo = scaled_kl - elogl + tf.cast(v_loss, dtype=tf.float64) * self.training_config['v_loss']
+                nelbo = - elogl #+ tf.cast(v_loss, dtype=tf.float64) * self.training_config['v_loss']
                 prediction = tf.argmax(smax, axis=1)
                 acc = tf.reduce_mean(tf.cast(tf.equal(prediction, t), dtype=tf.float32))
             else: